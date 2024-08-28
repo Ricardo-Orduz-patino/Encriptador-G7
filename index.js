4e9164aa19360fada324ef241620a9dda44c95d3
Ricardo-Orduz-patino <ricardo.alfonso.orduz.patino@gmail.com>
2024-08-27 23:55:23 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,59 @@
+// Función para encriptar el texto ingresado por el usuario
+function encriptar() {
+  // Obtener el texto del elemento HTML por su ID
+  let texto = document.getElementById("texto").value;
+  // Referencias a los elementos del DOM para mostrar mensajes y cambiar imágenes
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let muñeco = document.getElementById("muñeco");
+
+  // Reemplazo de vocales específicas por códigos predefinidos para encriptar
+  let textoCifrado = texto
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+
+  // Verificar si se ingresó texto para encriptar
+  if (texto.length != 0) {
+    document.getElementById("texto").value = textoCifrado;
+    tituloMensaje.textContent = "Texto encriptado con éxito";
+    parrafo.textContent = "";
+    muñeco.src = "./img/encriptado.jpg"; // Cambiar la imagen a una que indique éxito
+  } else {
+    muñeco.src = "./img/muñeco.png";
+    tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+    swal("Ooops!", "Debes ingresar un texto", "warning"); // Alerta para indicar que no se ingresó texto
+  }
+}
+
+// Función para desencriptar el texto encriptado
+function desencriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let muñeco = document.getElementById("muñeco");
+
+  // Revertir el proceso de encriptación reemplazando los códigos por sus respectivas vocales
+  let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+  
+  // Verificar si se ingresó texto para desencriptar
+  if (texto.length != 0) {
+    document.getElementById("texto").value = textoCifrado;
+    tituloMensaje.textContent = "Texto desencriptado con éxito";
+    parrafo.textContent = "";
+    muñeco.src = "./img/desencriptado.jpg"; // Cambiar la imagen a una que indique éxito en desencriptado
+  } else {
+    muñeco.src = "./img/muñeco.png";
+    tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+    swal("Ooops!", "Debes ingresar un texto", "warning"); // Alerta para indicar que no se ingresó texto
+  }
+}
